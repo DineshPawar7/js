2ead735dcfff02699b9c2d3084b9ccffb7ea51d5
DineshPawar7 <dsp70458@gmail.com>
2026-09-14 11:14:12 +0530

    dynamic content change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,27 +3,20 @@ import { useState } from "react"
 
 
 export default function App() {
-
-    let [count, setCount] = useState(0)
-
-    function increse() {
-        count ++
-
-        setCount(count)
-    }
+    const [content, setContent] = useState("dinesh not clickd")
 
 
+function handleClick() {
+    setContent("dinesh is clicked")
+
+}
 
     return (
-        <div>
+        <>
 
-            <div className=" items-center text-center justify-center bg-gray-200 rounded-lg">
+        <h1>{content}</h1>
 
-                <h1 className="font-bold">Counter: {count}</h1>
-
-                <button className="bg-black text-white" onClick={increse}>Click</button>
-
-            </div>
-        </div>
+        <button onClick={handleClick}>click</button>
+        </>
     )
 }
